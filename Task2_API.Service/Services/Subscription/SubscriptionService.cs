9258aff335480465fa2d0e03bb17acc9b7649685
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-03-20 00:26:02 +0200

    SubscriptionService updated

--- Task2_API.Service/Services/Subscription/SubscriptionService.cs
+++ Task2_API.Service/Services/Subscription/SubscriptionService.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Task2_API.Core.DTO;
 using Task2_API.Core.ViewModel;
 using Task2_API.Data;
+using Task2_API.Data.Models;
 
 namespace Task2_API.Service.Services.Subscription
 {
@@ -17,17 +20,38 @@ namespace Task2_API.Service.Services.Subscription
 
         public List<SubscriptionVM> Index()
         {
-            return null;
+            return _DB.Subscriptions.Include(x => x.Viewer).
+                Select(x => new SubscriptionVM() 
+                {
+                    Price = x.Price,
+                    SubscriptionDate = x.SubscriptionDate,
+                    SubscriptionTerm = x.SubscriptionTerm,
+                    Viewer = new ViewerVM()
+                    {
+                        Name = x.Viewer.Name,
+                        Email = x.Viewer.Email,
+                        RegistryDate = x.Viewer.RegistryDate
+                    }
+                }).ToList();
         }
 
         public SubscriptionVM Details(int id)
         {
-            return null;
+            return new SubscriptionVM(_DB.Subscriptions
+                .Include(x => x.Viewer).SingleOrDefault(x => x.Id == id));
         }
 
         public void Create(CreateSubscriptionDTO dTO)
         {
-
+            var subscription = new SubscriptionDbEntity()
+            {
+                ViewerId = dTO.ViewerId,
+                Price = dTO.Price,
+                SubscriptionDate = dTO.SubscriptionDate,
+                SubscriptionTerm = dTO.SubscriptionTerm,
+                IsDelete = false,
+                CreatedAt = DateTime.Now
+            };
         }
 
         public void Update(UpdateSubscriptionDTO dTO)
@@ -36,7 +60,9 @@ namespace Task2_API.Service.Services.Subscription
         }
         public void Delete(int id)
         {
-
+            var subscription = _DB.Subscriptions.Find(id);
+            subscription.IsDelete = true;
+            _DB.Subscriptions.Update(subscription);
         }
     }
 }
